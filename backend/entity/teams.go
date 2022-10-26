9972799f4f2e76334453ea8153ce658417695e9b
chowieuk <christopher.howie@gmail.com>
2022-10-25 21:35:20 -0700

    added teams entity

---
+++ backend/entity/teams.go
@@ -0,0 +1,26 @@
+package entity
+
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+// TeamResponse is used to construct requests and parse responses which include an array of teams
+type TeamResponse struct {
+	Error  string     `json:"error"`
+	Status string     `json:"status"`
+	Teams  []TeamData `json:"data"`
+}
+
+// TeamData is a reduced model of data associated with a team participating in the 2022 world cup
+// It essentially fits the data supplied from the API we're using for world cup data
+// https://github.com/raminmr/free-api-worldcup2022
+
+type TeamData struct {
+	ID       primitive.ObjectID `json:"_id" bson:"_id"`
+	Name     string             `json:"name_en" bson:"name_en"`
+	Flag     string             `json:"flag" bson:"flag"`
+	FifaCode string             `json:"fifa_code" bson:"fifa_code"`
+	ISO2     string             `json:"iso2" bson:"iso2"`
+	Group    string             `json:"groups" bson:"groups"`
+	Team_id  string             `json:"id" bson:"id"`
+}
