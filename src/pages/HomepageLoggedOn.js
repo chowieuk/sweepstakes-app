49971ba59fff4eb620eb178cb5c01e074a7b3a86
Patrick Reynolds <patrickreynoldscoding@gmail.com>
2022-11-03 16:52:04 +0700

    reg doesnt seem to work

--- src/pages/HomepageLoggedOn.js
+++ src/pages/HomepageLoggedOn.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 import "./Registration.css";
@@ -8,9 +8,28 @@ import { getUserData } from "../functions/getUserData";
 
 
 export default function HomePageLoggedOn() {
+  const [team, setTeam] = useState(false);
 
 
-  // getUserData()
+  const getUserData = async () => {
+  try {
+    const res = await fetch("http://localhost:8080/private_data", { //change endpoint as needed
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers":
+          "Origin, X-Requested-With, Content-Type, Accept",
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setTeam(data.userInfo.attrs.team_name);
+      });
+  } catch (err) {}
+};
+getUserData()
+
   return (
     <>
       <div className="post-reg-container">
@@ -19,7 +38,7 @@ export default function HomePageLoggedOn() {
         </div>
         <div className="post-reg-container-row2">
           <div className="post-reg-message">
-           Your team is {}!
+           Your team is {team}!
           </div>
         </div>
       </div>
