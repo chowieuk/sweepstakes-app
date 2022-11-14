5fbee5807762fbcd7e4c05e7c649f742d417ee30
chowieuk <christopher.howie@gmail.com>
2022-11-13 18:34:27 -0800

    Added flag and user name to /home

--- src/pages/HomepageLoggedOn.js
+++ src/pages/HomepageLoggedOn.js
@@ -6,21 +6,24 @@ import "./Registration.css";
 export default function HomePageLoggedOn() {
   const [team, setTeam] = useState(false);
   const [teamFlag, setTeamFlag] = useState(false);
+  const [userName, setUserName] = useState(false);
 
   const getUserData = async () => {
     try {
       const res = await fetch("http://localhost:8080/private_data", {
         //change endpoint as needed
         method: "GET",
+        credentials: 'include',
         headers: {
           "Content-Type": "application/json",
-          "Access-Control-Allow-Origin": "*",
-          "Access-Control-Allow-Headers":
-            "Origin, X-Requested-With, Content-Type, Accept",
+          // "Access-Control-Allow-Origin": "*",
+          // "Access-Control-Allow-Headers":
+          //   "Origin, X-Requested-With, Content-Type, Accept",
         },
       })
         .then((res) => res.json())
         .then((data) => {
+          setUserName(data.userInfo.name);
           setTeam(data.userInfo.attrs.team_name);
           setTeamFlag(data.userInfo.attrs.team_flag);
         });
@@ -33,11 +36,15 @@ export default function HomePageLoggedOn() {
       <div className="post-reg-container">
         <div className="post-reg-container-row1">
           <div className="post-reg-text">
-            Welcome to the World Cup 2022 sweepstakes!
+            Congratulations {userName}
           </div>
         </div>
         <div className="post-reg-container-row2">
-          <div className="post-reg-message">Your team is {team}!</div>
+          <div className="post-reg-message">
+            Your team is {team}!
+            <img className="flag-image" src={teamFlag}/>
+          </div>
+          
         </div>
       </div>
     </>
