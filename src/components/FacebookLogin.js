94526f50d34677666d9297d2cd80d1124d2a0981
chowieuk <christopher.howie@gmail.com>
2022-11-13 16:01:05 -0800

    Completed + styled social login buttons

--- src/components/FacebookLogin.js
+++ src/components/FacebookLogin.js
@@ -1,11 +1,9 @@
 import React from 'react'
 import {useRef, useEffect} from 'react';
 import {socialLogin, socialLoginErrorHandler} from '../functions/socialLogin';
-import useFacebookSDK from "../hooks/useFacebookSDK";
 
 const FacebookLogin = () => {
 
-  useFacebookSDK(); 
   const ref = useRef(null);
 
   useEffect(() => {
@@ -28,9 +26,9 @@ const FacebookLogin = () => {
   }, []);
 
   return (
-    <div>
-      <div ref={ref} class="fb-login-button" data-width="" data-size="large" data-button-type="continue_with" data-layout="default" data-auto-logout-link="false" data-use-continue-as="false"></div>
-    </div>
+    <button ref={ref} class="s-btn s-btn__icon s-btn__facebook bar-md" data-provider="facebook" data-oauthserver="https://www.facebook.com/v2.0/dialog/oauth" data-oauthversion="2.0">
+            <svg aria-hidden="true" class="svg-icon iconFacebook" width="18" height="18" viewBox="0 0 18 18"><path d="M3 1a2 2 0 0 0-2 2v12c0 1.1.9 2 2 2h12a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2H3Zm6.55 16v-6.2H7.46V8.4h2.09V6.61c0-2.07 1.26-3.2 3.1-3.2.88 0 1.64.07 1.87.1v2.16h-1.29c-1 0-1.19.48-1.19 1.18V8.4h2.39l-.31 2.42h-2.08V17h-2.5Z" fill="#4167B2"></path></svg>
+            &nbsp;Log in with Facebook</button>
   )
 }
 
